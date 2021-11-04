ea2782fff483a7129f54afb1334ad73255882d75
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2021-11-04 17:31:12 +0100

    Fix test

--- src/org/formacion/mediator/Mediator.java
+++ src/org/formacion/mediator/Mediator.java
@@ -1,9 +1,9 @@
 package org.formacion.mediator;
 
 public class Mediator {
-	
-	private Coche coche;
+
 	private Telefono telefono;
+	private Coche coche;
 	private Radio radio;
 	
 	public Mediator(Telefono telefono, Coche coche, Radio radio) {
@@ -14,26 +14,25 @@ public class Mediator {
 		this.coche.setMediator(this);
 		this.radio.setMediator(this);
 	}
-	
+
 	public void enciendeCoche() {
-		coche.enciende();
+		radio.enciende();
 		telefono.apagaMusica();
 	}
-	
+
 	public void apagaCoche() {
-		coche.apaga();
-	}
-	
-	public void recibeLlamada() {
 		radio.apaga();
 	}
-	
+
 	public void enciendeRadio() {
 		telefono.apagaMusica();
 	}
-	
+
 	public void apagaRadio() {
-		
 	}
-	
+
+	public void recibeLlamada() {
+		radio.apaga();
+	}
+
 }
